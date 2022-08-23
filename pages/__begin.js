6c534c3a8f51f1b3de4f1045361818eacd2cc94e
Sam Selikoff <sam.selikoff@gmail.com>
2022-08-22 22:47:19 -0400

    begin of video

--- pages/__begin.js
+++ pages/__begin.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { PlusIcon } from "@heroicons/react/solid";
 
 let seeds = [
   { user: "me", text: "Yo yo" },
@@ -20,22 +21,49 @@ seeds = seeds.map((seed, i) => ({ ...seed, id: i + 1 }));
 export default function Home() {
   const [messages, setMessages] = useState(seeds);
 
-  function removeMessage(mid) {
-    setMessages((messages) => messages.filter((message) => message.id !== mid));
+  function addMessage() {
+    let index = Math.floor(Math.random() * messages.length);
+    let newId = messages.length
+      ? Math.max(...messages.map((m) => m.id)) + 1
+      : 1;
+    let newMessage = {
+      id: newId,
+      user: Math.random() > 0.5 ? "me" : "them",
+      text: "Your mom said it's time to come home",
+    };
+
+    setMessages([
+      ...messages.slice(0, index),
+      newMessage,
+      ...messages.slice(index),
+    ]);
+  }
+
+  function removeMessage(message) {
+    setMessages((messages) => messages.filter((m) => m.id !== message.id));
   }
 
   return (
-    <div className="mt-8 px-8">
-      <ul className="max-w-sm mx-auto space-y-1 text-sm">
+    <div className="max-w-sm mx-auto flex flex-col px-4">
+      <div className="text-right mt-4">
+        <button
+          onClick={addMessage}
+          className="hover:bg-gray-100 active:bg-gray-200 rounded-full inline-flex items-center justify-center p-1.5 text-gray-500 hover:text-gray-700"
+        >
+          <PlusIcon className="w-4 h-4" />
+        </button>
+      </div>
+
+      <ul className="w-full space-y-1 mt-4 text-sm">
         {messages.map((message) => (
-          <li className="flex" key={message.id}>
+          <li key={message.id} className="flex">
             <button
-              onClick={() => removeMessage(message.id)}
+              onClick={() => removeMessage(message)}
               className={`${
                 message.user === "me"
                   ? "bg-blue-500 ml-auto"
                   : "bg-gray-500 mr-auto"
-              } px-3 py-1 bg-blue-500 text-white rounded-full`}
+              } px-3 py-1 bg-blue-500 text-white text-left rounded-full`}
             >
               {message.text}
             </button>
--- pages/__end.js
+++ pages/__end.js
@@ -1,5 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
+import { PlusIcon } from "@heroicons/react/solid";
 
 let seeds = [
   { user: "me", text: "Yo yo" },
@@ -20,34 +21,60 @@ seeds = seeds.map((seed, i) => ({ ...seed, id: i + 1 }));
 
 export default function Home() {
   const [messages, setMessages] = useState(seeds);
-  const [lastRemovedMessage, setLastRemovedMessage] = useState();
+  const [lastRemovedIndex, setLastRemovedIndex] = useState(null);
 
-  function removeMessage(mid) {
-    setLastRemovedMessage(mid);
-    setMessages((messages) => messages.filter((message) => message.id !== mid));
+  function addMessage() {
+    let index = Math.floor(Math.random() * messages.length);
+    let newId = messages.length
+      ? Math.max(...messages.map((m) => m.id)) + 1
+      : 1;
+    let newMessage = {
+      id: newId,
+      user: Math.random() > 0.5 ? "me" : "them",
+      text: "Your mom said it's time to come home",
+    };
+
+    setLastRemovedIndex(null);
+    setMessages([
+      ...messages.slice(0, index),
+      newMessage,
+      ...messages.slice(index),
+    ]);
   }
 
-  let animatingMessages = messages.filter(
-    (message) => message.id > lastRemovedMessage
-  );
+  function removeMessage(message) {
+    setLastRemovedIndex(messages.indexOf(message));
+    setMessages((messages) => messages.filter((m) => m.id !== message.id));
+  }
+
+  let animatingMessages =
+    lastRemovedIndex !== null ? messages.slice(lastRemovedIndex) : [];
 
   return (
-    <div className="mt-8 px-8">
-      <ul className="max-w-sm mx-auto space-y-1 text-sm">
-        <AnimatePresence mode="popLayout">
+    <div className="max-w-sm mx-auto flex flex-col px-4">
+      <div className="text-right mt-4">
+        <button
+          onClick={addMessage}
+          className="hover:bg-gray-100 active:bg-gray-200 rounded-full inline-flex items-center justify-center p-1.5 text-gray-500 hover:text-gray-700"
+        >
+          <PlusIcon className="w-4 h-4" />
+        </button>
+      </div>
+
+      <ul className="w-full space-y-1 mt-4 text-sm">
+        <AnimatePresence mode="popLayout" initial={false}>
           {messages.map((message) => (
             <motion.li
               layout
-              exit={{
-                opacity: 0,
-                scale: 0.9,
-              }}
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.9 }}
               transition={{
                 default: { duration: 0.15 },
                 layout: {
                   type: "spring",
                   bounce: 0.4,
-                  // duration: 0.85,
+                  duaration: 1,
                   duration: animatingMessages.includes(message)
                     ? 0.15 * animatingMessages.indexOf(message) + 0.85
                     : 1,
@@ -61,12 +88,12 @@ export default function Home() {
               key={message.id}
             >
               <button
-                onClick={() => removeMessage(message.id)}
+                onClick={() => removeMessage(message)}
                 className={`${
                   message.user === "me"
                     ? "bg-blue-500 ml-auto"
                     : "bg-gray-500 mr-auto"
-                } px-3 py-1 bg-blue-500 text-white rounded-full`}
+                } px-3 py-1 bg-blue-500 text-white text-left rounded-full`}
               >
                 {message.text}
               </button>
